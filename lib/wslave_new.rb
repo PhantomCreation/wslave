6d5c0bdb21a617af5855ebf5e7b366c385c32155
影月 零 <zero@phantom.industries>
2017-09-10 20:41:50 +0900

    fix new generator

--- lib/wslave_new.rb
+++ lib/wslave_new.rb
@@ -25,14 +25,19 @@ class WSlaveNew
     `cd #{path} && git submodule add git://github.com/WordPress/WordPress.git public/wordpress`
     `cd #{path}/public/wordpress && git checkout #{version}-branch` if version != ''
     `cd #{path} && git submodule update --init --recursive public/wordpress`
+
+    puts "  > Preparing detached content directory"
     FileUtils.cp_r("#{path}/public/wordpress/wp-content", "#{path}/public/wp-content")
-    FileUtils.mkdir("#{path}/public/wordpress/wp-content/uploads")
-    FileUtils.touch("#{path}/public/wordpress/wp-content/uploads/.gitkeep")
-    FileUtils.mkdir("#{path}/public/wordpress/wp-content/upgrade")
-    FileUtils.touch("#{path}/public/wordpress/wp-content/upgrade/.gitkeep")
+    FileUtils.mkdir("#{path}/public/wp-content/uploads")
+    FileUtils.touch("#{path}/public/wp-content/uploads/.gitkeep")
+    FileUtils.mkdir("#{path}/public/wp-content/upgrade")
+    FileUtils.touch("#{path}/public/wp-content/upgrade/.gitkeep")
     Dir.chdir path
+
+    puts "  > Setting permissions"
     WSlaveTools.set_dev_perms
     
     `cd #{path} && git add --all && git commit -am "Add and initialize WordPress#{version}"`
+    puts "  > Done!"
   end
 end
