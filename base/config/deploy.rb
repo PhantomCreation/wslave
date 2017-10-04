d56433f32b7af5591d4eb97d626986d1232f2170
影月 零 <zero@phantom.industries>
2017-10-04 22:44:42 +0900

    fix deploy switching

--- base/config/deploy.rb
+++ base/config/deploy.rb
@@ -6,3 +6,12 @@ opts = YAML.load_file('config/definitions.yml')
 
 set :application, opts['app']['name']
 set :repo_url, opts['app']['repo']
+
+deploy_method = opts['options']['deploy_method'] if opts.has_key?('options') &&
+  opts['options'].has_key?('deploy_method')
+
+case deploy_method
+when 'scp'
+  set :scm, :copy
+else
+end
