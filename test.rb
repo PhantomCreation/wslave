1fa7002ab49889871b6ae930807f346899a99191
zero <zero@phantom.industries>
2020-03-01 17:13:42 +0900

    nweb "working", needs site config

--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 puts ">> Changing directory to ensure isolation"
 Dir.chdir(__dir__)
 puts ">> Attempting to use wslave command to create an installation:"
-`#{__dir__}/bin/wslave new tmp --wspath ./wstmp`
-Dir.chdir("#{__dir__}/tmp")
-`#{__dir__}/bin/wslave server`
+`#{__dir__}/bin/wslave new testing --wspath ../`
+Dir.chdir("#{__dir__}/testing")
+#`#{__dir__}/bin/wslave server`
+`bundle exec wslave server`
