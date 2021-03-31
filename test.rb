eafae4c272c731283b4d3e9d2211afeb2f99b941
影月 零 <zero@phantom.industries>
2021-03-31 17:21:01 +0900

    wip: version detection working, nginx not working

--- test.rb
+++ test.rb
@@ -2,15 +2,16 @@ puts ">> Changing directory to ensure isolation."
 Dir.chdir(__dir__)
 
 puts ">> Attempting to use wslave command to create an installation..."
-`#{__dir__}/bin/wslave new testing --wspath ../ --version 5.4`
+#`#{__dir__}/bin/wslave new testing --wspath ../ --version 5.4`
+`#{__dir__}/bin/wslave new testing --wspath ../`
 puts ">> wslave installation created."
 Dir.chdir("#{__dir__}/testing")
 
 puts ">> Starting server..."
-`bundle exec wslave server start`
+#`bundle exec wslave server start`
 
 puts ">> Creating Sage theme named \"test\"..."
-`bundle exec wslave sage create test`
+#`bundle exec wslave sage create test`
 
 puts ">> Attempting to install theme dependencies and build..."
-`bundle exec wslave sage update`
+#`bundle exec wslave sage update`
