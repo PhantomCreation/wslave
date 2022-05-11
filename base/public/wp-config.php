13974b18370650d5449a869dddfaecd3327f91f5
影月 零 <zero@phantom.industries>
2022-05-11 13:48:30 +0900

    wip, fixed php-fpm sock file issue and streamlined php-fpm version def

--- base/public/wp-config.php
+++ base/public/wp-config.php
@@ -1,51 +1,19 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
-
-// ** MySQL settings - You can get this info from your web host ** //
+/** DB Info */
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
-
 /** MySQL database username */
 define('DB_USER', 'root');
-
 /** MySQL database password */
 define('DB_PASSWORD', 'wordpress');
-
 /** MySQL hostname */
 define('DB_HOST', 'db');
-
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+/** Auth Filler */
 define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
 define('LOGGED_IN_KEY',    'put your unique phrase here');
@@ -82,14 +50,15 @@ define('WP_DEBUG', false);
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/wordpress/');
+if (!defined('ABSPATH'))
+  define('ABSPATH', dirname(__FILE__) . '/wordpress');
 
 define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
-define('WP_CONTENT_DIR', realpath(ABSPATH . '../wp-content/'));
+define('WP_SITEURL', WP_HOME . '/wordpress');
+define('WP_CONTENT_DIR', realpath(ABSPATH . '../wp-content'));
 define('WP_CONTENT_URL', WP_HOME . '/wp-content');
 //define('WP_TEMP_DIR', realpath(ABSPATH . '../../tmp/'));
+
 define('FS_METHOD', 'direct');
 
 define('FORCE_SSL_ADMIN', false);
--- test.rb
+++ test.rb
@@ -21,8 +21,8 @@ Dir.chdir("#{__dir__}/testing")
 puts ">> Starting server..."
 `bundle exec wslave server start`
 
-puts ">> Creating Sage theme named \"test\"..."
-`bundle exec wslave sage create test`
+#puts ">> Creating Sage theme named \"test\"..."
+#`bundle exec wslave sage create test`
 
-puts ">> Attempting to install theme dependencies and build..."
-`bundle exec wslave sage update`
+#puts ">> Attempting to install theme dependencies and build..."
+#`bundle exec wslave sage update`
