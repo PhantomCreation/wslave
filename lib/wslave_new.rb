ba1d978981266198bc2a438db9b42c8ff1d358a3
Rika Yoshida <nazuki@phantom.industries>
2023-12-12 10:57:44 +0900

    fix rubocop settings, refactor

--- lib/wslave_new.rb
+++ lib/wslave_new.rb
@@ -25,13 +25,13 @@ class WSlaveNew
       end
     end
 
-    FileUtils.mkdir_p path
+    FileUtils.mkdir_p(path)
 
-    Dir.chdir path
+    Dir.chdir(path)
 
     puts "  > Setting up WordPress WSlave setup in: #{path}"
-    FileUtils.cp_r Dir.glob("#{base_path}/*"), path
-    FileUtils.cp_r Dir.glob("#{template_path}/*"), path
+    FileUtils.cp_r(Dir.glob("#{base_path}/*"), path)
+    FileUtils.cp_r(Dir.glob("#{template_path}/*"), path)
     add_path_to_gemspec(wspath, path) if manual_path
 
     spec = Gem::Specification.load("#{wspath}/wslave.gemspec")
@@ -39,29 +39,9 @@ class WSlaveNew
 
     `cd #{path} && git init && git add --all && git commit -am "initial commit by wslave"`
 
-    if wppath != ''
-      wppath = File.expand_path(wppath)
-      puts "  >> Checking wppath (#{wppath}) ..."
-      if File.directory? wppath
-        puts '  >> wppath is a folder. Copying...'
-        Dir.chdir path
-        FileUtils.cp_r wppath, 'public/wordpress'
-        Dir.chdir 'public/wordpress'
-        `git clean -fdx; git stash`
-        `git checkout master`
-        `git pull`
-      end
-    end
+    copy_wordpress_from_wppath(path, wppath) if wppath != ''
 
-    `cd #{path} && git submodule add https://github.com/WordPress/WordPress.git public/wordpress`
-    `cd #{path} && git submodule update --init --recursive public/wordpress`
-    if %w[edge master].include?(version)
-      `cd #{path}/public/wordpress && git checkout master`
-    elsif version != ''
-      `cd #{path}/public/wordpress && git checkout #{version}`
-    else
-      `cd #{path}/public/wordpress && git checkout #{get_stable_branch_version("#{path}/public/wordpress")}-branch`
-    end
+    add_submodule(path, version)
 
     puts '  > Preparing detached content directory'
     FileUtils.cp_r("#{path}/public/wordpress/wp-content", "#{path}/public/wp-content")
@@ -69,7 +49,7 @@ class WSlaveNew
     FileUtils.touch("#{path}/public/wp-content/uploads/.gitkeep")
     FileUtils.mkdir_p("#{path}/public/wp-content/upgrade")
     FileUtils.touch("#{path}/public/wp-content/upgrade/.gitkeep")
-    Dir.chdir path
+    Dir.chdir(path)
 
     puts '  > Preparing static data directory'
     FileUtils.mkdir_p("#{path}/public/data")
@@ -87,9 +67,35 @@ class WSlaveNew
     File.open("#{path}/Gemfile", 'w') { |gemfile| gemfile.puts gemtext }
   end
 
+  def add_submodule(path, version)
+    `cd #{path} && git submodule add https://github.com/WordPress/WordPress.git public/wordpress`
+    `cd #{path} && git submodule update --init --recursive public/wordpress`
+    if %w[edge master].include?(version)
+      `cd #{path}/public/wordpress && git checkout master`
+    elsif version != ''
+      `cd #{path}/public/wordpress && git checkout #{version}`
+    else
+      `cd #{path}/public/wordpress && git checkout #{get_stable_branch_version("#{path}/public/wordpress")}-branch`
+    end
+  end
+
+  def copy_wordpress_from_wppath(path, wppath)
+    wppath = File.expand_path(wppath)
+    puts "  >> Checking wppath (#{wppath}) ..."
+    return unless File.directory?(wppath)
+
+    puts '  >> wppath is a folder. Copying...'
+    Dir.chdir(path)
+    FileUtils.cp_r(wppath, 'public/wordpress')
+    Dir.chdir('public/wordpress')
+    `git clean -fdx; git stash`
+    `git checkout master`
+    `git pull`
+  end
+
   def get_stable_branch_version(path)
-    latest_major = 5
-    latest_minor = 7
+    latest_major = 6
+    latest_minor = 4
 
     reg = /^(\d*)\.(\d)-branch$/
     puts "> Checking for WordPress versions in: #{path}"
